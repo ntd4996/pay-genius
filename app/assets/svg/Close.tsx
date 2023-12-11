9ed4b71a7c4e4cd3975023e6108abebc714db795
datnt <ntd4996@gmail.com>
2023-12-11 10:46:53 +0700

    fix: feedback

--- app/assets/svg/Close.tsx
+++ app/assets/svg/Close.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Close() {
   return (
     <svg
-      className='h-6 w-6 text-gray-800 dark:text-white'
+      className='h-6 w-6 text-white'
       aria-hidden='true'
       xmlns='http://www.w3.org/2000/svg'
       fill='currentColor'
